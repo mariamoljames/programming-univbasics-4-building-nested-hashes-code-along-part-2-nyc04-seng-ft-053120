5fb4942cc7b8c3997c7a71050b719414483c5626
Mariamol James <mariamol.james@gmail.com>
2020-06-09 16:13:33 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -27,7 +27,6 @@ end
 
 def add_characters
   update_status
-  additional_characters=[{name:"Prince Escalus"},{name:"Apothecary"}]
-  epic_tragedy<<additional_characters
+  epic_tragedy[:additional_characters]=[{name:"Prince Escalus"},{name:"Apothecary"}]
   epic_tragedy
 end
