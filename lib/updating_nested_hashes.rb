4669c20ccb9dff319b8bc0617ba84427b56fd745
Mariamol James <mariamol.james@gmail.com>
2020-06-09 16:01:49 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -25,3 +25,31 @@ epic_tragedy[montague][hero][status]="dead"
 epic_tragedy[capulet][heroine][status]="dead"
 epic_tragedy
 end
+
+def update_status
+
+  epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+      {name: "Steven", age: "30", attitude: "confused"},
+      {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+   }
+}
+
+additional_characters=[{name:"Prince Escalus"},{name:"Apothecary"}]
+epic_tragedy[
+epic_tragedy
+end
